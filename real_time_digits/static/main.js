621030a0585ddd5c860f169c9fcec33f164b6bf6
Slava <v4slava.guk@gmail.com>
2024-12-15 17:15:12 +0300

    front done

--- real_time_digits/static/main.js
+++ real_time_digits/static/main.js
@@ -1,7 +1,31 @@
+let ctx = document.querySelector('#myChart')
+
+let graphData = {
+    type: 'line',
+    data: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+    },
+}
+
+let myChart = new Chart(ctx, graphData)
+
 var socket = new WebSocket('ws://localhost:8000/ws/some-url')
 
 socket.onmessage = function(event){
     var data = JSON.parse(event.data)
     console.log(data)
-    document.querySelector('#app').innerText = data.message
+
+    let newGraphData = graphData.data.datasets[0].data
+    newGraphData.shift()
+    newGraphData.push(data.message)
+
+    graphData.data.datasets[0].data = newGraphData
+    myChart.update()
 }
